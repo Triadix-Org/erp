5bd2979b6de6b5e1bea036bb75f0fb73dfbb1c64
Ega Bagus Purnama <egabagus1@gmail.com>
2025-08-13 21:11:33 +0700

    Add detailed itemization and totals to proforma invoice; include tax, shipment price, and signature section

--- resources/views/pdf/invoice/proforma.blade.php
+++ resources/views/pdf/invoice/proforma.blade.php
@@ -24,7 +24,85 @@
             </div>
         </div>
 
+        <table border="0" cellspacing="0" cellpadding="0" class="detail-table" style="font-size: 9pt;">
+            <thead>
+                <tr>
+                    <th style="width: 5%" class="no">#</th>
+                    <th style="width: 40%" class="desc">ITEM</th>
+                    <th style="width: 20%" class="unit">PRICE</th>
+                    <th style="width: 10%" class="qty">QTY</th>
+                    <th style="width: 25%" class="unit">TOTAL PRICE</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($detail as $details)
+                    <tr>
+                        <td class="no">{{ $loop->iteration }}</td>
+                        <td class="desc">
+                            {{ $details['product']['name'] }}
+                        </td>
+                        <td class="unit right-column">
+                            Rp {{ number_format($details['product']['price'], 0, ',', '.') }}
+                        </td>
+                        <td class="qty">{{ $details['qty'] }}</td>
+                        <td class="unit right-column">
+                            Rp {{ number_format($details['price_total'], 0, ',', '.') }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
+        <table border="0" cellspacing="0" cellpadding="0" class="total-table" style="font-size: 9pt;">
+            <tbody>
+                <tr>
+                    <td width="50%">Tax</td>
+                    <td class="unit right-column" width="50%">
+                        Rp {{ number_format($total_tax, 0, ',', '.') }}
+                    </td>
+                </tr>
+                <tr>
+                    <td width="50%">Shipment Price</td>
+                    <td class="unit right-column" width="50%">
+                        Rp {{ number_format($shipment_price, 0, ',', '.') }}
+                    </td>
+                </tr>
+                <tr>
+                    <td width="50%">Total Amount</td>
+                    <td class="unit right-column" width="50%">
+                        Rp {{ number_format($total_amount, 0, ',', '.') }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="sign">
+            <table class="signature-table">
+                <thead>
+                    <tr>
+                        <th width="60%"></th>
+                        <th>Best Regards,</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><img class="user-sign" src="{{ public_path('storage/' . $user['sign']) }}" alt="ttd">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>{{ $user['name'] }}</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td></td>
+                        <td>Sales & Marketing</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
     </main>
     @include('pdf.partials.footer')
 </body>
